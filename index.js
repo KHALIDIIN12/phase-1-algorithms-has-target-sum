f2a406e5fb8f00ff84e6aa7f6fbc722225f5ad2f
KHALIDIIN12 <khalidinho53@gmail.com>
2025-06-27 14:02:51 +0300

    Complete Has Target Sum Lab

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+
+  for (const num of array) {
+    const complement = target - num;
+    if (seen.has(complement)) return true;
+    seen.add(num);
+  }
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
-
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true  (pair is 5 & 5)");
+  console.log("=>", hasTargetSum([5, 5, 3], 10));
+
+  console.log("Expecting: false (empty array)");
+  console.log("=>", hasTargetSum([], 4));
 }
+
 
 module.exports = hasTargetSum;
